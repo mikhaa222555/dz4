291f177f1bf7df1130baf12a8bb12df3409b2e12
smple <smple@DESKTOP-QBR57AP>
2024-07-25 22:00:34 +0300

    Добавьте файлы проекта.

---
+++ dz4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Тема 15. Урок 1. Одномерный массив.
+string[] stroks = new string[11];
+stroks[0] = " освоении ";
+stroks[1] = " остальное";
+stroks[2] = " по плечу";
+stroks[3] = " сложный ";
+stroks[4] = " но если ";
+stroks[5] = " программирования";
+stroks[6] = " многие бросают";
+stroks[7] = " остальное будет";
+stroks[8] = " освоить их,";
+stroks[9] = " этап в";
+stroks[10] = " на этом этапе ,Массивы -";
+
+
+for (int i = 0; i < stroks.Length; i++)
+{
+    Console.Write(stroks[i]);
+}
